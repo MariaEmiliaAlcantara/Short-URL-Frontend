201b5027ae2137c447fe7a05cb7d50827a818deb
MariaSchinoni <maria.schinoni10@gmail.com>
2022-11-09 14:44:21 -0300

    start project

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/top-urls">Top 100</router-link>
+    <router-link to="/login">Login</router-link>
+    <router-link to="/signup">Sign up</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
